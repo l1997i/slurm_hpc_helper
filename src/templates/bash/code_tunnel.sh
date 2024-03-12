e722771cbe32ce36d51d5ad021e3fac1ebcdcfc6
Luis Li <li.li4@durham.ac.uk>
2024-03-12 14:59:50 +0000

    feat: update code tunnel scripts

--- src/templates/bash/code_tunnel.sh
+++ src/templates/bash/code_tunnel.sh
@@ -7,7 +7,7 @@ mkdir -p .logs/runner/${SLURM_JOB_ID}
 nohup code tunnel --accept-server-license-terms --name "${host}-${SLURM_JOB_ID}" --cli-data-dir ~/.jobs/${date}_${SLURM_JOB_ID} > .logs/runner/${SLURM_JOB_ID}/runner-${SLURM_JOB_ID}.log 2>&1 &
 server_pid=$!
 content="Slurm job name: ${SLURM_JOB_NAME}. 
-Online dev: https://vscode.dev/tunnel/${host}-${SLURM_JOB_ID}$(pwd). 
+Online dev: https://vscode.dev/tunnel/${host}-${SLURM_JOB_ID}$(pwd)    Server PID: ${server_pid}. 
 Slurm jobs overview: http://ncc.clients.dur.ac.uk/grafana/d/5UwAWAzWk/slurm-jobs?orgId=1&var-user=$(whoami)&var-job=All&from=now-6h&to=now. 
 To get the code, please run the command remotely: 
 > more $(pwd)/.logs/runner/${SLURM_JOB_ID}/runner-${SLURM_JOB_ID}.log | grep -o 'code [A-Z0-9-]*' | tail -n 1
